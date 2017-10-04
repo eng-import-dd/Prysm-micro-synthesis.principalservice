a2be9969a011398316937d95cd08c4c13e05f4ab
Charan <cmalyala@prysm.com>
2017-10-04 10:58:07 +0530

    BUild isuues fixed

--- Synthesis.PrincipalService.Modules/Requests/CreateMachineRequest.cs
+++ Synthesis.PrincipalService.Modules/Requests/CreateMachineRequest.cs
@@ -29,7 +29,5 @@ namespace Synthesis.PrincipalService.Requests
         public string SynthesisVersion { get; set; }
          
         public DateTime? LastOnline { get; set; }
-        public TimeSpan AveragePingTime { get; set; }
-        public bool CurrentlyOnline { get; set; }
     }
 }
