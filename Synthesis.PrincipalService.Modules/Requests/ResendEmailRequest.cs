d0c0db236e91481b1f288d980171eaf10238c16b
Charan <cmalyala@prysm.com>
2017-10-09 15:51:56 +0530

    Fixed review comments

--- Synthesis.PrincipalService.Modules/Requests/ResendEmailRequest.cs
+++ Synthesis.PrincipalService.Modules/Requests/ResendEmailRequest.cs
@@ -5,16 +5,10 @@ namespace Synthesis.PrincipalService.Requests
 {
     public class ResendEmailRequest
     {
-        public Guid? Id { get; set; }
 
         public string FirstName { get; set; }
 
-        public string LastName { get; set; }
-
         public string Email { get; set; }
 
-        public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
-
-        public string Initials { get { return string.Format("{0}{1}", FirstName?.ToUpper().FirstOrDefault(), LastName.ToUpper().FirstOrDefault()); } }
     }
 }
