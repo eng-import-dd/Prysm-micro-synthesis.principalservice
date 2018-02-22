74eb2df4e2efd3ce45dd7b3712cb7ee918fb471c
Devendra Turkar <dturkar@prysm.com>
2018-02-22 18:30:49 +0530

    Initial Commit

--- Synthesis.Principal.Modules.Test/Modules/UsersModuleTests.cs
+++ Synthesis.Principal.Modules.Test/Modules/UsersModuleTests.cs
@@ -209,7 +209,7 @@ namespace Synthesis.PrincipalService.Modules.Test.Modules
             _controllerMock.Setup(m => m.GetUsersForAccountAsync(It.IsAny<GetUsersParams>(), It.IsAny<Guid>(), It.IsAny<Guid>()))
                 .Returns(Task.FromResult(new PagingMetadata<UserResponse>()));
 
-            var response = await UserTokenBrowser.Get("/api/v1/users/", BuildRequest);
+            var response = await UserTokenBrowser.Post("/api/v1/account/users", BuildRequest);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
@@ -219,7 +219,7 @@ namespace Synthesis.PrincipalService.Modules.Test.Modules
             _controllerMock.Setup(m => m.GetUsersForAccountAsync(It.IsAny<GetUsersParams>(), It.IsAny<Guid>(), It.IsAny<Guid>()))
                 .Throws(new NotFoundException(string.Empty));
 
-            var response = await UserTokenBrowser.Get("/api/v1/users/", BuildRequest);
+            var response = await UserTokenBrowser.Post("/api/v1/account/users", BuildRequest);
 
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
@@ -229,7 +229,7 @@ namespace Synthesis.PrincipalService.Modules.Test.Modules
             _controllerMock.Setup(m => m.GetUsersForAccountAsync(It.IsAny<GetUsersParams>(),It.IsAny<Guid>(), It.IsAny<Guid>()))
                 .Throws(new ValidationFailedException(new List<ValidationFailure>()));
 
-            var response = await UserTokenBrowser.Get("/api/v1/users/", BuildRequest);
+            var response = await UserTokenBrowser.Post("/api/v1/account/users", BuildRequest);
 
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
@@ -239,7 +239,7 @@ namespace Synthesis.PrincipalService.Modules.Test.Modules
             _controllerMock.Setup(m => m.GetUsersForAccountAsync(It.IsAny<GetUsersParams>(), It.IsAny<Guid>(), It.IsAny<Guid>()))
                 .Returns(Task.FromResult(new PagingMetadata<UserResponse>()));
 
-            var response = await UnauthenticatedBrowser.Get("/api/v1/users/", BuildRequest);
+            var response = await UnauthenticatedBrowser.Post("/api/v1/account/users", BuildRequest);
 
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
@@ -249,7 +249,7 @@ namespace Synthesis.PrincipalService.Modules.Test.Modules
             _controllerMock.Setup(m => m.GetUsersForAccountAsync(It.IsAny<GetUsersParams>(), It.IsAny<Guid>(), It.IsAny<Guid>()))
                 .Throws(new Exception());
 
-            var response = await UserTokenBrowser.Get("/api/v1/users/", BuildRequest);
+            var response = await UserTokenBrowser.Post("/api/v1/account/users", BuildRequest);
 
             Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
         }
