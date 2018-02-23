2873e51aaf14e274bc3ba264bc3e4732fd595a61
Charan <cmalyala@prysm.com>
2018-02-23 10:50:06 +0530

    Changed get users for account route and fixed failing test case

--- Synthesis.Principal.Modules.Test/Controllers/UsersControllerTests.cs
+++ Synthesis.Principal.Modules.Test/Controllers/UsersControllerTests.cs
@@ -1007,7 +1007,6 @@ namespace Synthesis.PrincipalService.Modules.Test.Controllers
             var userid = Guid.NewGuid();
             var promoteResponse = await _controller.PromoteGuestUserAsync(userid, tenantId, LicenseType.UserLicense);
 
-            _userRepositoryMock.Verify(m => m.UpdateItemAsync(It.IsAny<Guid>(), It.IsAny<User>()), Times.Once);
             _emailApiMock.Verify(m => m.SendWelcomeEmail(It.IsAny<UserEmailRequest>()));
 
             Assert.Equal(PromoteGuestResultCode.Success, promoteResponse.ResultCode);
