a16a16d8dedb53f37611f3eee4c16c191a411906
Stephan Nagy <snagy@prysm.com>
2020-06-15 17:33:57 -0400

    Update registrations

--- Synthesis.Principal.Modules/PrincipalAutofacModule.cs
+++ Synthesis.Principal.Modules/PrincipalAutofacModule.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using Autofac;
 using Autofac.Core;
 using AutoMapper;
+using Nancy;
 using Synthesis.Configuration;
 using Synthesis.DocumentStorage;
 using Synthesis.EmailService.InternalApi.Api;
@@ -21,6 +22,7 @@ using Synthesis.PrincipalService.Email;
 using Synthesis.PrincipalService.Events;
 using Synthesis.PrincipalService.InternalApi.Models;
 using Synthesis.PrincipalService.Mapper;
+using Synthesis.PrincipalService.Modules;
 using Synthesis.PrincipalService.Services;
 using Synthesis.ProjectService.InternalApi.Api;
 using Synthesis.SubscriptionService.InternalApi.Api;
@@ -65,20 +67,26 @@ namespace Synthesis.PrincipalService
                 .WithParameter(new ResolvedParameter(
                     (p, c) => p.Name == "deploymentType",
                     (p, c) => c.Resolve<IAppSettingsReader>().GetValue<string>("Principal.DeploymentType")));
+
             builder.RegisterType<UserInvitesController>().As<IUserInvitesController>();
             builder.RegisterType<MachinesController>().As<IMachinesController>();
             builder.RegisterType<GroupsController>().As<IGroupsController>();
             builder.RegisterType<SuperAdminService>().As<ISuperAdminService>();
 
+            // Api
             builder.RegisterType<LicenseApi>().As<ILicenseApi>();
             builder.RegisterType<TenantApi>().As<ITenantApi>();
             builder.RegisterType<ProjectAccessApi>().As<IProjectAccessApi>();
             builder.RegisterType<IdentityUserApi>().As<IIdentityUserApi>();
             builder.RegisterType<CloudShim>().As<ICloudShim>();
-            builder.RegisterType<TenantUserSearchBuilder>().As<ITenantUserSearchBuilder>();
+            builder.RegisterType<EmailApi>()
+                .WithParameter("serviceUrlSettingName", "Email.Url")
+                .As<IEmailApi>();
+            builder.RegisterType<EmailSendingService>().As<IEmailSendingService>().InstancePerRequest();
+            builder.RegisterType<SubscriptionApi>()
+                .As<ISubscriptionApi>();
 
-            builder.RegisterType<UserQueryRunner>().As<IQueryRunner<User>>();
-
+            // Health Reporters
             builder.RegisterType<RepositoryHealthReporter<User>>().As<IHealthReporter>()
                 .SingleInstance();
 
@@ -91,13 +99,17 @@ namespace Synthesis.PrincipalService
             builder.RegisterType<RepositoryHealthReporter<Group>>().As<IHealthReporter>()
                 .SingleInstance();
 
-            builder.RegisterType<EmailApi>()
-                .WithParameter("serviceUrlSettingName", "Email.Url")
-                .As<IEmailApi>();
-            builder.RegisterType<EmailSendingService>().As<IEmailSendingService>().InstancePerRequest();
-            builder.RegisterType<SubscriptionApi>()
-                .As<ISubscriptionApi>();
-            
+            // other
+            builder.RegisterType<TenantUserSearchBuilder>().As<ITenantUserSearchBuilder>();
+            builder.RegisterType<UserQueryRunner>().As<IQueryRunner<User>>();
+
+            // nancy modules
+            builder.RegisterType<GroupsModule>().As<INancyModule>();
+            builder.RegisterType<MachinesModule>().As<INancyModule>();
+            builder.RegisterType<PrincipalServiceHealthModule>().As<INancyModule>();
+            builder.RegisterType<UserInviteModule>().As<INancyModule>();
+            builder.RegisterType<UsersModule>().As<INancyModule>();
+
             //Feature Flags
             builder.Register(c =>
             {
@@ -121,7 +133,6 @@ namespace Synthesis.PrincipalService
                 })
                 .As<IFeatureFlagProvider>()
                 .SingleInstance();
-
         }
     }
 }
